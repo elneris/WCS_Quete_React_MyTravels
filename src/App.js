00944c29723e3d0415e8a589c51ab2feebab4754
Elneris Dang <elneris.dang@gmail.com>
2019-05-05 10:26:40 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
+import React, { Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Travel from "./Travel";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Là ou j'ai eu la chance d'aller déjà</h1>
+        </header>
+        <Travel
+          destination="Kununurra"
+          distance = "14 293 km"
+          country="Australie"
+          photo="https://content.knightfrank.com/property/3594600/images/1724c83e-ee90-4992-9ec7-2da363890fd9-73.jpg"
+        />
+        <Travel
+          destination="New York"
+          distance="5 798 km"
+          country="Etats-Unis"
+          photo="https://www.sensationsdumonde.com/img/destination/87/skyline_nyc__empire_state_building__shutterstock_2018_768a.jpg"
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
